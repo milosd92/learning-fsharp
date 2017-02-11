26e5b48b5f8ec4c9f8697522f592181b22c58247
milosd92 <milosdanilov@gmail.com>
2017-02-11 18:33:05 +0100

    writen a parser

--- Parser/Parser.fs
+++ Parser/Parser.fs
@@ -1,4 +1,29 @@
-namespace Parser
+module StockChartBot.Parser
 
-type Parser() = 
-    member this.X = "F#"
+open System
+open System.Globalization
+
+type Query =
+    {
+        Sender : string
+        Ticker : string
+        From : DateTime
+        To : DateTime
+    }
+
+let private parseDate (s : string) =
+    DateTime.ParseExact(
+        s, 
+        "M/d/yyyy", 
+        CultureInfo.InvariantCulture, 
+        DateTimeStyles.AssumeLocal
+    )
+
+let Parse (text : string) =
+    let elements = text.Split([|' '|])
+    {
+        Sender = elements.[0]
+        Ticker = elements.[1]
+        From = elements.[2] |> parseDate
+        To = elements.[3] |> parseDate
+    }
--- Tests/Parser.Tests/Parser.Tests.fs
+++ Tests/Parser.Tests/Parser.Tests.fs
@@ -3,9 +3,18 @@ module StockChartBot.Parser.Tests
 open System
 open NUnit.Framework
 
-type ``This is a test``() =
+open StockChartBot.Parser
+
+type ``Given some valid text``() =
     [<Test>]
-    member this.``my first test works``() =
-        let expected = true
-        let actual = true
+    member this.``the parser parses the text into sender, ticker, from and to``() =
+        let text = "@StockChartBot LNKD 1/1/2000 12/31/2015"
+        let expected = 
+            {
+                Sender = "@StockChartBot"
+                Ticker = "LNKD"
+                From = DateTime(2000, 1, 1)
+                To = DateTime(2015, 12, 31)
+            }
+        let actual = Parse text
         Assert.AreEqual(expected, actual)
