e4b107ace833d7787670838f1287ebb2cad10f1d
milosd92 <milosdanilov@gmail.com>
2017-02-17 18:04:52 +0100

    added *.projitems

---
+++ Common/Choice.fs
@@ -0,0 +1,16 @@
+﻿module StockChartBot.Rop
+
+[<RequireQualifiedAccess>]
+module Choice =
+    // Success or failure
+    type Result<'S, 'F> =
+        | Success of 'S
+        | Failure of 'F
+
+    // Convert a single value into a success two-track result
+    let succeed x =
+        Success x
+
+    // Convert a single value into a failure two-track result	
+    let fail x =
+        Failure x	
